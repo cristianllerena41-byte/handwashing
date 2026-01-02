3ab19950a9ff813127e95c3a54e1f21f60336d0e
cristianllerena41-byte <cristianllerena41@gmail.com>
2026-01-01 17:37:52 -0800

    Update handwashing.py

--- handwashing.py
+++ handwashing.py
@@ -9,7 +9,7 @@
 #   streamlit run app.py
 
 import pandas as pd
-import plotly.express as px 
+import plotly.express as px
 import streamlit as st
 
 st.set_page_config(page_title="Semmelweis Clinics Dashboard", layout="wide")
